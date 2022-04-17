319b1c95c3f5a1f5a2f10399c19185fda1607bcc
EricEOL <lopes.eric051@gmail.com>
2022-04-16 21:40:47 -0300

    Adicionando validação as classes de formulário

--- src/main/java/me/dio/academia/digital/entity/form/MatriculaForm.java
+++ src/main/java/me/dio/academia/digital/entity/form/MatriculaForm.java
@@ -1,7 +1,12 @@
 package me.dio.academia.digital.entity.form;
 
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Positive;
+
 public class MatriculaForm {
 
+  @NotNull
+  @Positive
   private Long alunoId;
 
 }
